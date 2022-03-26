b8a52530dd8ea485525ca5fe685f63e2afb44d0f
annieasryan <msasryan88@gmail.com>
2022-03-26 22:41:21 +0400

    feat(winner): doesCartWinPrize

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const WINNING_SUM = 123.40;
 @param {Array} cartItemsPrices
 */
 function doesCartWinPrize(cartItemsPrices) {
-  
+    let sum = cartItemsPrices.reduce((acc, item) => Number(acc) + Number(item))
+    let strSum = sum + ''
+    let arrSum = strSum.split('.')
+    if (arrSum[1].length > 2) {
+        sum = Math.round(sum * 10) / 10
+    }
+    return sum === WINNING_SUM
 }
+
 
 module.exports = doesCartWinPrize;
